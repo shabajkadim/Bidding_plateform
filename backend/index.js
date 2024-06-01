c3f5ac287acbedf01ffbb074127989091ae800d4
Shahabaj kadim <shahabajkadim7@gmail.com>
2024-06-01 18:54:11 +0530

    updates

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv'
 import mongoose from 'mongoose'
 
 const app=express()
-app.use(express.json())
+app.use(express.json({ limit: "10mb" }))
 app.use(cors())
 dotenv.config() 
 
